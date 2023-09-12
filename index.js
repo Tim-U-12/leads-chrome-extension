cb01a90fdac6112f2e69962ed3b3f49c6d1d7072
Tim <tim.a.ung.12@gmail.com>
2023-09-12 20:24:28 +1000

    created a function that renders and saves the leads

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function saveInput() {
-    console.log("Input Saved")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById('input-btn')
+const ulEl = document.getElementById('ulEl')
+
+inputBtn.addEventListener('click', function() {
+    myLeads.push(inputEl.value)
+    renderLeads(inputEl.value)
+})
+
+function renderLeads(value) {
+    // ulEl.innerHtml += "<li>" + value + "</li>"
+    const li = document.createElement('li')
+    li.textContent = value
+    ulEl.append(li)
 }
+
+
