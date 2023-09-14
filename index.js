07f3c3284f89e15259bdaf9300422a2d617db17d
Tim <tim.a.ung.12@gmail.com>
2023-09-14 13:28:17 +1000

    added a button with the ability to save the url of the current active tab

--- index.js
+++ index.js
@@ -1,25 +1,56 @@
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById('input-btn')
+const tabBtn = document.getElementById('tab-btn')
+const deleteBtn = document.getElementById('delete-btn')
 const ulEl = document.getElementById('ulEl')
+
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    renderLeads(myLeads, ulEl)
+}
 
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    renderLeads()
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads(myLeads, ulEl)
 })
 
-function renderLeads() {
+
+tabBtn.addEventListener('click', function(){
+    // chrome.tabs.query({active: true,lastFocusedWindow: true}, function(tabs) {
+        
+    // })
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        renderLeads(myLeads, ulEl)
+    })
+})
+
+
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.setItem('myLeads', JSON.stringify([]))
+    myLeads = []
+    renderLeads(myLeads, ulEl)
+})
+
+function renderLeads(leads, htmlElement) {
     let listItems = ""
-    for (i = 0; i < myLeads.length; i++) {
-        //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    for (i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
     }
-    ulEl.innerHTML = listItems
+    htmlElement.innerHTML = listItems
 }
